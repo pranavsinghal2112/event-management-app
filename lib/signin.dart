146ff53da8aae3866710136d6db1acb56e582517
Pranav <pranavsinghal2112@gmail.com>
2023-01-26 00:10:12 +0530

    Added my project

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,3 +1,4 @@
+import 'package:first/start.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,9 +8,62 @@ class signin extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          height: 200,
-          child: SvgPicture.asset("assets/signin.svg"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              child: SvgPicture.asset("assets/signin.svg"),
+            ),
+            Container(
+              width: 250,
+              child: SizedBox(
+                height: 40,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              child: Text(
+                "forget Password ?",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => start()));
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Don't have an account?  ",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+            )
+          ],
         ),
       ),
       appBar: AppBar(
---
+++ lib/start.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 200,
+            child: SvgPicture.asset('assets/start1.svg'),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(onPressed: (){}, child: Text('you want to organise')),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            height: 200,
+            child: SvgPicture.asset('assets/start2.svg'),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(onPressed: (){}, child: Text('you want to be contractor')),
+
+        ],
+      ),
+      appBar: AppBar(
+          title: Center(
+            child: Text(
+        'EvesAndGo',
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+      ),
+          )
+      ),
+    );
+    
+  }
+}
